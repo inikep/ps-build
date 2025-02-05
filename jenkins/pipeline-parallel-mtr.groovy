0fc3648d8cb013644ed8d76ffc0333714f15b7f0
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2025-02-05 11:08:23 +0100

    PS-9500: Improve `jenkins/pipeline-parallel-mtr.groovy`
    1. Use POSIX sh syntax
    2. Simplify and describe the script in `prepareWorkspace`

--- jenkins/pipeline-parallel-mtr.groovy
+++ jenkins/pipeline-parallel-mtr.groovy
@@ -66,11 +66,15 @@ void prepareWorkspace(Integer WORKER_ID) {
         sh """
             echo "prepareWorkspace for MTR worker ${WORKER_ID}"
             sudo git reset --hard
-            if [[ "${WORKER_ID}" != "1" ]]; then
-                sudo git clean -xdf
+
+            # it's safe to use "git clean" as "sources/" are ignored with ".gitignore"
+            sudo git clean -xdf
+
+            # sources exists only for WORKER #1 but not for retry/re-runs
+            if [ -d sources ]; then
+                sudo git -C sources reset --hard
+                sudo git -C sources clean -xdf
             fi
-            sudo git -C sources reset --hard || :
-            sudo git -C sources clean -xdf   || :
 
             if [ -f /usr/bin/yum ]; then
                 sudo yum -y install jq gflags-devel
